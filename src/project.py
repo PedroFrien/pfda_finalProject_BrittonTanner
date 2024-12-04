af0511e8773444b2c6ae8ce6bce3dd6301d84e66
PedroFrien <tannerbredstonechicken@outlook.com>
2024-12-04 00:42:30 -0600

    Updated the visualsto be animated with the pieces falling into place when the player clicks on a certain column.

--- src/project.py
+++ src/project.py
@@ -24,44 +24,40 @@ class FallingPiece:
         self.piece_type = piece_type
         self.squaresize = squaresize
 
-        
         if self.piece_type == 1:
             self.color = RED
         elif self.piece_type == 2:
-            self.color = YELLOW  # Fixed this line
+            self.color = YELLOW
 
         self.width = screen.get_width()
         self.height = screen.get_height()
         self.x = int(col * squaresize + squaresize / 2)
         self.y = squaresize / 2
-        
-       
-        self.final_y = final_y[1] if isinstance(final_y, tuple) else final_y
 
+        self.final_y = final_y[1] if isinstance(final_y, tuple) else final_y
         self.falling_speed = random.uniform(0, 0.01)
-        self.acceleration = 0.001
+        self.acceleration = 0.005
 
         self.is_complete = False
 
-    def update(self):
+        self.surface = pygame.Surface((squaresize, squaresize), pygame.SRCALPHA)
 
-        if self.is_complete == False:
+    def update(self):
+        if not self.is_complete:
             self.falling_speed += self.acceleration
             self.y += self.falling_speed
 
-        if self.final_y is not None:
-            self.is_complete = self.y >= self.final_y
-        else: 
-            self.is_complete = self.y > self.height
+            if self.y >= self.final_y:
+                self.y = self.final_y
+                self.is_complete = True
 
-        return self.is_complete
-    
     def draw(self):
-
-        # surf = pygame.Surface((SQUARESIZE, SQUARESIZE))
-        pygame.draw.circle(self.screen, self.color, (self.x, int(self.y)), int(self.squaresize/2-5))
-        # surf.blit
-        pygame.display.update()
+        self.surface.fill((0, 0, 0, 0))
+        pygame.draw.circle(self.surface, self.color, (self.squaresize // 2, self.squaresize // 2), int(self.squaresize / 2 - 5))
+    # Blit the surface onto the screen at the correct position
+        self.screen.blit(self.surface, (self.x - self.squaresize // 2, self.y - self.squaresize // 2))
+        # pygame.display.update()
+        
 
 def create_board():
  
@@ -131,7 +127,11 @@ size = (width, height)
 screen = pygame.display.set_mode(size)
 
 def main():
+    
 
+
+    current_player = 1
+    posx = 1
     board = create_board()
     print_board(board)
 
@@ -147,18 +147,18 @@ def main():
     # size = (width, height)
 
 
-    draw_board(screen, SQUARESIZE, board)
+    draw_board(screen, SQUARESIZE, board, posx, current_player)
     pygame.display.update()
 
-    current_player = 1
-    posx = 0
+    
+    
     
     while not game_over:
 
         
         
         
-        draw_board(screen, SQUARESIZE, board)
+        draw_board(screen, SQUARESIZE, board, posx, current_player)
 
         for piece in falling_pieces:
             piece.update()
@@ -167,14 +167,12 @@ def main():
         
 
         # Update falling pieces
-        falling_pieces[:] = [piece for piece in falling_pieces if not piece.is_complete]
-        
-        pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+        falling_pieces[:] = [piece for piece in falling_pieces]
 
-        if current_player == 1:
-            pygame.draw.circle(screen, RED, (int(posx), int(SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
-        else:
-            pygame.draw.circle(screen, YELLOW, (int(posx), int(SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
+        
+       
+        
+        
          
         
         
@@ -186,7 +184,7 @@ def main():
             
             if event.type == pygame.MOUSEMOTION: 
                 posx = event.pos[0]
-                # pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE)) 
+                 
 
                 
                 pygame.display.update()
@@ -209,30 +207,40 @@ def main():
                     turn += 1
                     current_player = turn % 2 + 1
                     print_board(board)
-                    draw_board(screen, SQUARESIZE, board)
+                    draw_board(screen, SQUARESIZE, board, posx, current_player)
 
-def draw_board(screen, SQUARESIZE, board):
+def draw_board(screen, SQUARESIZE, board, posx, current_player):
+
+    pygame.draw.rect(screen, BLACK, (0, 0, width, SQUARESIZE))
+    if current_player == 1:
+        pygame.draw.circle(screen, RED, (int(posx), int(SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
+    else:
+        pygame.draw.circle(screen, YELLOW, (int(posx), int(SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
     # Background
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
             pygame.draw.rect(screen, BLUE, (c * SQUARESIZE, r * SQUARESIZE + SQUARESIZE, SQUARESIZE, SQUARESIZE))  
     
     # # Player Pieces
-    for c in range(COLUMN_COUNT):
-        for r in range(ROW_COUNT):
-            if board[r][c] == 1:
-                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE + SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
-            elif board[r][c] == 2:
-                pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE + SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
+    # for c in range(COLUMN_COUNT):
+    #     for r in range(ROW_COUNT):
+    #         if board[r][c] == 1:
+    #             pygame.draw.circle(screen, RED, (int(c*SQUARESIZE + SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
+    #         elif board[r][c] == 2:
+    #             pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE + SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
+    
+
     
     # Holes
     for c in range(COLUMN_COUNT):
         for r in range(ROW_COUNT):
-            if board[r][c] == 0:
-                pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE + SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
+            # if board[r][c] == 0:
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE + SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)),int(SQUARESIZE/2 - 5))
 
-                
+    for piece in falling_pieces:
+            piece.draw()
 
+    
     pygame.display.update()
 
 
